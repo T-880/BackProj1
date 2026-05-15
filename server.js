b94f3d7ac663ab298b18fd899f6731b229cb0cfd
Trella <mr.trella@hotmail.com>
2026-05-15 15:28:16 +0200

    Skapat grundstruktur för backend och konfigurerat Express-servern

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Testar route
+app.get("/", (req, res) => {
+  res.send("Forno Nero API körs!");
+});
+
+// Startar server
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
